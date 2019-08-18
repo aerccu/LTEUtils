acfeac4158e8f3b80be285c4ee7515ec155226ec
aerccu <aerccu@gmail.com>
2019-08-18 23:23:26 +0200

    minor changes

--- include/processing.h
+++ include/processing.h
@@ -16,3 +16,12 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+
+#ifndef PROCESSING_H
+#define PROCESSING_H
+
+#include <cmath>
+#include <complex>
+
+#endif
